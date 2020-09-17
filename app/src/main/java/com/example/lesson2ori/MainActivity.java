47a3766d80e96d7d3bd7bd962a3f957fc4865125
OriOfek <aa6055@bs.amalnet.k12.il>
2020-09-17 11:14:34 +0300

    seconnd activity design

--- app/src/main/java/com/example/lesson2ori/MainActivity.java
+++ app/src/main/java/com/example/lesson2ori/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.lesson2ori;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    Switch s;
+    EditText etn;
+    Intent si;
+    EditText etd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        s = (Switch) findViewById(R.id.sw);
+        etn = (EditText)findViewById(R.id.etn);
+        etd = (EditText)findViewById(R.id.etd);
+    }
+
+    public void results(View view) {
+        si = new Intent(this,results.class);
+        si.putExtra("type",s.isChecked());
+        si.putExtra("d",etd.getText());
+        si.putExtra("n",etn.getText());
+        startActivity(si);
     }
 }
